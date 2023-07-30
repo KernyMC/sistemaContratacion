05ac770a796da4c83f6deec8b08f05eaa32a77cd
Genesis-Calapaqui <gicalapaqui@espe.edu.ec>
2023-07-29 23:48:16 -0500

    Update PopUpRegistro.jsx

--- sistemaContratacion-main/src/components/PopUps/PopUpRegistro.jsx
+++ sistemaContratacion-main/src/components/PopUps/PopUpRegistro.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import useState from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -8,7 +9,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Paper from '@mui/material/Paper';
 import Draggable from 'react-draggable';
 import TextField from '@mui/material/TextField';
-
+import axios from 'axios';
 
 function PaperComponent(props) {
   return (
@@ -26,6 +27,10 @@ function PaperComponent(props) {
 export default function PopUpRegistro() {
   const [open, setOpen] = React.useState(false);
 
+  const [otp, setOTP] = React.useState('');
+  const [otpInput, setOTPInput] = React.useState('');
+
+
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -33,10 +38,42 @@ export default function PopUpRegistro() {
   const handleClose = () => {
     setOpen(false);
   };
+  
 
+  const handleSendOTP = () => {
+    // Generar un número aleatorio como OTP
+    const otp_val = Math.floor(Math.random() * 10000);
+    setOTP(otp_val)
+   //console.log(email,otp);
+    axios.post('http://localhost:8800/send-email', {email: localStorage.getItem('email'), otp: otp_val})
+      .then((response) => {
+        alert(response.data.message);
+    })
+      .catch((error) => {
+        console.error(error);
+        alert('Error sending email');
+      });
+  };
+
+  const handleVerifyOTP = () => {
+    if(otpInput == otp){
+      alert("Correo electrónico verificado...");
+    }
+    else{
+        alert("Código inválido");
+    }
+  };
+  const handleClickOpenAndSendOTP = () => {
+    handleClickOpen();
+    handleSendOTP();
+  }
+  const handleCloseAndVerifyOTP = () => {
+    handleClose();
+    handleVerifyOTP();
+  }
   return (
     <div>
-      <Button variant="outlined" onClick={handleClickOpen}>
+      <Button variant="outlined" onClick={handleClickOpenAndSendOTP}>
         Aceptar
       </Button>
       <Dialog
@@ -52,12 +89,12 @@ export default function PopUpRegistro() {
           <DialogContentText>
             Ingrese el codigo enviado a su correo
           </DialogContentText>
-          <TextField id="filled-basic" label="Código" variant="filled" 
+          <TextField id="filled-basic" label="Código" variant="filled" onChange={(e) => setOTPInput(e.target.value)}
           style={{ display: 'flex', justifyContent: 'center' }}
           />    
           </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}>Validar</Button>
+          <Button onClick={handleCloseAndVerifyOTP}>Validar</Button>
         </DialogActions>
       </Dialog>
     </div>
